271a4c8e03116d86c7969a8cf8617cdb73016367
kuhciob <44519595+kuhciob@users.noreply.github.com>
2020-07-10 15:39:39 +0300

    Update AppSettings.cs

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -7,9 +7,8 @@ namespace SMoDABot.Models
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://smadcalc.azurewebsites.net:443/{0}";
-        public static string Url2 { get; set; } = "https://smadcalc.azurewebsites.net/{0}";
+        public static string Url { get; set; } = "";
         public static string Name { get; set; } = "smoda_bot";
-        public static string Key { get; set; } = "1049740066:AAHLmSZ7I11XfNiK4NFbs9mRRFbnHgIgGKo";
+        public static string Key { get; set; } = "";
     }
 }
